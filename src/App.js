9f0a8291988a8f3755186a668f9b209e68223fe8
Parsa Vesali <parsavesali84@gmail.com>
2024-02-13 19:06:55 +0330

    sidebar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import SideBar from './Components/SideBar/SideBar'
 export default function App() {
   return (
-    <div>سلام</div>
+    <SideBar />
   )
 }
---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import CommentIcon from "@mui/icons-material/Comment";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import "./SideBar.css";
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+      <span className="sidebar__logo">
+        <h2 className="sidebar__logo-text">MY.PANEL</h2>
+        <img
+          className="sidebar__logo-img"
+          src="./img/svg/logo.png"
+          alt="logo"
+        />
+      </span>
+
+      <ul className="sidebar-list">
+        <li className="sidebar-item active">
+          <a href="#" className="sidebar-link">
+            <DashboardIcon />
+            داشبورد
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#" className="sidebar-link">
+            <TrendingUpIcon />
+            تجزیه و تحلیل
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#" className="sidebar-link">
+            <AddShoppingCartIcon />
+            محصولات
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#" className="sidebar-link">
+            <CommentIcon />
+            نظرات
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#" className="sidebar-link">
+            <PeopleAltIcon />
+            کاربران
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#" className="sidebar-link">
+            <LocalMallIcon />
+            سفارشات
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#" className="sidebar-link">
+            <MonetizationOnIcon />
+            تخفیف ها
+          </a>
+        </li>
+      </ul>
+      
+    </div>
+  );
+}
